8b0ce34b4962c08726028fb7f213cb462644d454
Khanyi-Codes <makhubukhanyisile1@gmail.com>
2026-05-05 10:46:18 +0200

    Feat: Adding APi functionality

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function displayPoem(response){
     cursor: "",
     delay: 1,
   }); 
-  
+
 }
 
 function generatePoem(event){
@@ -16,7 +16,7 @@ function generatePoem(event){
     let instrctionsInput  = document.querySelector("#user-instructions")
     let apiKey = "c64t23cdba0aa619b751fda43e0ao4bf";
     let prompt = `Please generate an english poem about ${instrctionsInput.value}`
-    let context = "You are a romantic poem expert and love to write short poem ,your mission is to generate a four line poem in basic html. Only write the poem and nothing else Make sure to use the user instructions"
+    let context = "You are a romantic poem expert and love to write short poem ,your mission is to generate a four line poem in html tags. Only write the poem and nothing else Make sure to use the user instructions"
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
     
     console.log(prompt)
